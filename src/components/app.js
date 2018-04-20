ebd8f4f36addf5f76bc5202163e039d11e62e14e
Nathan Cooper <nac33@students.uwf.edu>
2018-04-19 21:18:20 -0500

    Added container for book list and reducer for book titles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello there!
+        <BookList />
       </div>
     );
   }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,6 @@ export default function () {
         { title: 'Javascript: The Good Parts'},
         { title: 'Title 2'},
         { title: 'Title 3'},
-        { title: 'Title 4'},
+        { title: 'Title 4'}
     ];
 }
