88976fafd9b82b6e2817029bd1815b3142e7c6b2
Nathan Cooper <nac33@students.uwf.edu>
2018-04-19 20:38:07 -0500

    Initial boilerplate project

---
+++ src/reducer/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The Good Parts'},
+        { title: 'Title 2'},
+        { title: 'Title 3'},
+        { title: 'Title 4'},
+    ];
+}
